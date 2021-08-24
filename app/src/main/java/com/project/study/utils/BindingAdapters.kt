86c8550f30406cb748b375abca3016db0867c69d
IW_Dnyaneshwar <dnyaneshwar@iwizardsolutions.com>
2021-08-24 19:18:22 +0530

    - Added @BindingConversion

--- app/src/main/java/com/project/study/utils/BindingAdapters.kt
+++ app/src/main/java/com/project/study/utils/BindingAdapters.kt
@@ -3,7 +3,9 @@ package com.project.study.utils
 import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.databinding.BindingConversion
 import com.bumptech.glide.Glide
 import com.project.study.data.model.Urls
 
@@ -12,11 +14,13 @@ fun View.visibleGone(value: Boolean) {
     visibility = if (value) View.VISIBLE else View.GONE
 }
 
+@BindingConversion
+fun convertBooleanToVisibility(value: Boolean): Int {
+    return if (value) View.VISIBLE else View.GONE
+}
+
 @BindingAdapter(value = ["imageSrc", "placeholder"], requireAll = false)
-fun ImageView.imageUrl(
-    url: String?,
-    placeholder: Drawable? = null
-) {
+fun ImageView.imageUrl(url: String?, placeholder: Drawable? = null) {
     val photoUrl = url?.stringToObject<Urls>()?.regular
     Glide.with(context)
         .load(photoUrl)
